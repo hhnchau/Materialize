edc14a08eff5a514523ebffd7b3c2a4f7a6c85d5
Minh Long <minhlong49th@gmail.com>
2018-04-05 17:54:47 +0700

    add server get image

--- app/server.js
+++ app/server.js
@@ -1,12 +1,28 @@
 var express = require('express');
 var app = express();
-app.listen(3000, function(){
+var path = require('path');
+
+app.listen(3000, function () {
   console.log("Connect successfull!");
 });
 
 app.set("view engine", "ejs");
-app.set("views", "../views");
+app.set("views", "../views/html");
 
-app.get("/index", function(req, res){
-  res.render("html/index");
+app.get("/index", function (req, res) {
+  res.render("index", { url: "http://localhost:3000/" });
+});
+
+
+app.get('/image', function (req, res) {
+  res.sendFile(path.resolve("../views/images/hotel/aloc.jpg"));
+});
+
+
+app.get('/css', function (req, res) {
+  res.sendFile(path.resolve("../views/css/materialize.css"));
+});
+
+app.get('/article:id', function(req , res){
+  console.log('article' + req.params.id); 
 });
